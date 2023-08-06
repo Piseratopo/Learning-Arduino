058bf4143c9b840a8012f61a3e387cbced8b55ce
Piceratop Finfina <buinhatminh2k52005@gmail.com>
2023-08-06 16:26:48 +0700

    Add Blink

--- Blink/src/Blink.cpp
+++ Blink/src/Blink.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 
 int LED_PIN = 13;
+int delayTime = 1000;
 
 void setup()
 {
@@ -10,10 +11,10 @@ void setup()
 
 void loop()
 {
-    digitalWrite(LED_PIN, HIGH);
-    delay(250);
     Serial.println("LED on");
-    digitalWrite(LED_PIN, LOW);
-    delay(250);
+    digitalWrite(LED_PIN, HIGH);
+    delay(delayTime);
     Serial.println("LED off");
+    digitalWrite(LED_PIN, LOW);
+    delay(delayTime);
 }
